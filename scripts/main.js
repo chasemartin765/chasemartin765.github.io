210e2c4b34e17046ef3e168e286acdc58ba48448
chasemartin765 <144712814+chasemartin765@users.noreply.github.com>
2024-04-26 16:47:14 +0000

    completed tasks except 8

--- scripts/main.js
+++ scripts/main.js
@@ -32,4 +32,39 @@ for (let i = 1; i <= 10; i++) {
     console.log(factorial);
 }
 
+console.log("Task 6")
+let arr = [3, 1, 4, 1, 5]
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+console.log("Task 7")
+for (let i = arr.length - 1; i >= 0; i--){
+    console.log(arr.at(i))
+}
+    
+
+console.log("Task 8");
+let sum2 = [];
+for (let i = 1; i <= 10; i++) {
+    sum2.push(i * i * i);
+}
+
+
+console.log("Task 9");
+let fibonacciNumbers = [];
+fibonacciNumbers[0] = 0;
+fibonacciNumbers[1] = 1;
+for (let i = 2; i < 10; i++) {
+    fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+}
+console.log(fibonacciNumbers);
+
+
+console.log("Task 10");
+let reversedArr = [];
+for (let i = arr.length - 1; i >= 0; i--) {
+    reversedArr.push(arr[i]);
+}
+console.log(reversedArr);
 
