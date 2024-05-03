8174f49140b79a5f5e6b3a7013a40ec89f3e7658
chasemartin765 <144712814+chasemartin765@users.noreply.github.com>
2024-05-03 17:00:22 +0000

    completed

---
+++ scripts/assignment.js
@@ -0,0 +1,10 @@
+let colorArr = ["red", "silver", "black", "maroon", "lime", "teal", "aqua"];
+let ul = document.createElement("ul");
+colors.forEach(color => {
+    let li = document.createElement("li");
+    li.innerText = color;
+    li.style.color = color;
+    ul.appendChild(li);
+ });
+ document.body.appendChild(ul);
+ 
